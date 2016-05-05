e4082b0fb0ac71eb959ca0e68654c0db26c1bbf9
Andrew Clemons <andrew.clemons@powershop.co.nz>
2016-05-05 15:25:51 +1200

    Drop copyright headers from file
    Seems most ruby projects just have LICENSE and don't have headers in
    each file

--- spec/services/make_guess_spec.rb
+++ spec/services/make_guess_spec.rb
@@ -1,30 +1,11 @@
-#
-# Copyright (C) 2016 Powershop New Zealand Ltd
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
-# the Software without restriction, including without limitation the rights to
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-# the Software, and to permit persons to whom the Software is furnished to do so,
-# subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
 require 'rails_helper'
 
 RSpec.describe MakeGuess do
 
   let(:initial_lives) { 2 }
   let(:initial_status) { GameStatus::STATUS_NEW }
-  let(:game) { Game.new(word: 'Ruby', lives: initial_lives) }
+  let(:word) { "Ruby" }
+  let(:game) { Game.new(word: word, lives: initial_lives) }
   let(:id) { game.id }
 
   before do
